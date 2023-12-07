6078b379d61a15a564e3000e8d1dc9739d6aa48a
yxxh020 <yiran023h@gmail.com>
2023-12-07 09:26:19 +0900

    useParams, useSearchparams, useNavigate

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
+  const { id } = useParams(); //사용자 정의 custom hook
+  console.log(id);
   return (
     <div>
       <h1>Diary</h1>
-      <p>this is Diary detaile page</p>
+      <p>this is Diary detail page</p>
     </div>
   );
 };
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,38 @@
-const Edit = ()=>{
-    return <div>
-        <h1>Edit</h1>
-        <p>Edit page</p>
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id:", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode:", mode);
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>Edit page</p>
+      <button onClick={() => setSearchParams({ who: "hwang" })}>
+        query 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Home으로
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        go back
+      </button>
     </div>
-}
+  );
+};
 
 export default Edit;
